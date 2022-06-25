aa8a0c1fa0cf1c4c12e66a481233a8512e739f28
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-06-25 15:13:55 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -58,7 +58,7 @@ class App extends React.Component {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          fontSize: 40,
+          fontSize: 30,
           color: '#010101',
         }}
       >
